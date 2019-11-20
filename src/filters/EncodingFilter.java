7d1c1c743cc808119ee72531a6c084083ce7a122
rioccom <373ryo0911@gmail.com>
2019-11-20 13:56:43 +0900

    redo

---
+++ src/filters/EncodingFilter.java
@@ -0,0 +1,36 @@
+package filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/*")    //////////////////////////////以下をフィルター処理として登録
+public class EncodingFilter implements Filter {
+	
+	public EncodingFilter() {  ////////////////////デフォルトコンストラクタ
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+	
+}
+
+
