5f8396d97d22e40d0899aad0d79db99d7d050d89
rioccom <373ryo0911@gmail.com>
2019-11-26 21:45:37 +0900

    Add Report new

--- WebContent/WEB-INF/views/reports/new.jsp
+++ WebContent/WEB-INF/views/reports/new.jsp
@@ -1,11 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>Insert title here</title>
-    </head>
-    <body>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-    </body>
-</html>
+<c:import url="/WEB-INF/views/layout/app.jsp">
+	<c:param name="content">
+	
+		<h2>日報　新規登録ページ</h2>
+
+		<form method="POST" action="<c:url value='/reports/create' />">
+			<c:import url="_form.jsp" /> <%--フォームのパーツを挿入する --%>
+		</form>
+
+		<p><a href="<c:url value='/reports/index' />">一覧に戻る</a></p>
+		
+	</c:param>
+</c:import>
--- src/controllers/reports/ReportsNewServlet.java
+++ src/controllers/reports/ReportsNewServlet.java
@@ -1,33 +1,35 @@
 package controllers.reports;
 
 import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ReportsNewServlet
- */
-@WebServlet(name = "reports/new", urlPatterns = { "/reports/new" })
-public class ReportsNewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReportsNewServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+import models.Report;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+@WebServlet("/reports/new")
+public class ReportsNewServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ReportsNewServlet() {
+		super();
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("_token", request.getSession().getId());    //セッションスコープの@Idをgetして、リクエストスコープの属性_tokenに代入する
+		
+		Report r = new Report();                                                //Report型オブジェクトr を生成
+		r.setReport_date(new Date(System.currentTimeMillis()));         //r のｾｯﾀｰを通して、本日の日付を取得して格納
+		request.setAttribute("report", r);                                       //リクエストスコープの属性reportにオブジェクトrを格納
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp"); //new.jspにフォワード
+		rd.forward(request, response);
 	}
 
 }
